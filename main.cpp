14c95a8a67b40a73274e454d28bf51743c93b677
Gerardo Aceves <Luis.AcevesS@alumnos.udg.mx>
2020-09-30 17:37:55 -0500

    prueba01

--- main.cpp
+++ main.cpp
@@ -3,7 +3,34 @@
 using namespace std;
 
 int main() {
-    cout << "Hola Mundo!" << endl; // eliminar esta línea
+    string cad;
+
+    getline(cin, cad);
+    if (cad == "char"){
+
+        cout << sizeof(char) << endl;
+    }
+    else if(cad == "int"){
+
+        cout << sizeof(int) << endl;
+    }
+    else if(cad == "short"){
+
+        cout << sizeof(short) << endl;
+    }
+    else if(cad == "float"){
+
+        cout << sizeof(float) << endl;
+    }
+    else if(cad == "double"){
+
+        cout << sizeof(double) << endl;
+    }
+    else if(cad == "long"){
+
+        cout << sizeof(long) << endl;
+    }
+    
 
     return 0;
 }
